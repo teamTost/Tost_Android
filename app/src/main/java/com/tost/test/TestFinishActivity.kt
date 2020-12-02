4fea59891830c21677f297913a5e49ec6f994494
yunakim2 <98_02@naver.com>
2020-12-02 22:53:19 +0900

    [Feat] 모의고사 시작 , 끝 뷰 완성

---
+++ app/src/main/java/com/tost/test/TestFinishActivity.kt
@@ -0,0 +1,12 @@
+package com.tost.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tost.R
+
+class TestFinishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_finish)
+    }
+}
