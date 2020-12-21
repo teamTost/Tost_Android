5aa8af85fec924352b791faa084a877ef104a45c
Malibin <nightmare73@naver.com>
2020-12-21 23:54:19 +0900

    [Feat] 목표 설정 Layout 작성

---
+++ app/src/main/java/com/tost/presentation/goal/GoalActivity.kt
@@ -0,0 +1,11 @@
+package com.tost.presentation.goal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goal)
+    }
+}
