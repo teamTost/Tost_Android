8332c464d7fe157f8528a3878e192adbf0fae3ba
Malibin <nightmare73@naver.com>
2020-12-30 23:55:40 +0900

    [Feat] 복습하기 Layout 작성

---
+++ app/src/main/java/com/tost/presentation/review/ReviewActivity.kt
@@ -0,0 +1,12 @@
+package com.tost.presentation.review
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tost.R
+
+class ReviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review)
+    }
+}
