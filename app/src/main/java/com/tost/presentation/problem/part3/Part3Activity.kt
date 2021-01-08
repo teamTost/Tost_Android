a66300583c2bed6f12ac31d9ee5d976c6fd418a0
Malibin <nightmare73@naver.com>
2021-01-08 22:58:27 +0900

    [Feat] Part3 코드 복제

---
+++ app/src/main/java/com/tost/presentation/problem/part3/Part3Activity.kt
@@ -0,0 +1,165 @@
+package com.tost.presentation.problem.part3
+
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import com.tost.R
+import com.tost.data.entity.ProblemState
+import com.tost.databinding.ActivityPart3Binding
+import com.tost.presentation.problem.ProblemGuideActivity
+import com.tost.presentation.problem.base.AudioBaseActivity
+import com.tost.presentation.problem.dialog.StopTalkingButtonsDialog
+import com.tost.presentation.problem.part6.Part6Activity
+import com.tost.presentation.problem.widget.AudioStateButton
+import com.tost.presentation.utils.showToast
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.concurrent.TimeUnit
+
+@AndroidEntryPoint
+class Part3Activity : AudioBaseActivity(), AudioStateButton.OnClickListener {
+    private var binding: ActivityPart3Binding? = null
+    private val part3ViewModel: Part3ViewModel by viewModels()
+
+    private var problemPlayer: MediaPlayer? = null
+    private var speakingNoticePlayer: MediaPlayer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityPart3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        this.binding = binding
+
+        initView(binding)
+        if (previousPermissionGranted()) onInitialPermissionGranted()
+        else askAudioPermission()
+
+        part3ViewModel.problem.observe(this) { startProblem() }
+    }
+
+    private fun initView(binding: ActivityPart3Binding) {
+        speakingNoticePlayer = MediaPlayer.create(this, R.raw.begin_speaking_now)
+        binding.lifecycleOwner = this
+        binding.viewModel = part3ViewModel
+        binding.problemToolBar.setOnCloseClickListener { finish() }
+        binding.buttonAudioController.setOnStateClickListener(this)
+        binding.buttonRestart.setOnClickListener { cancelRecord() }
+        binding.buttonSkip.setOnClickListener { skipPreparation() }
+        binding.buttonNext.setOnClickListener { startNextProblem() }
+        part3ViewModel.toastMessage.observe(this) { showToast(it) }
+    }
+
+    override fun onInitialPermissionGranted() {
+        part3ViewModel.loadProblem(getProblemNumber())
+    }
+
+    override fun onAudioButtonClick(state: AudioStateButton.State) = when (state) {
+        AudioStateButton.State.RECORDING -> startRecord(RESPONSE_TIME_15)
+        AudioStateButton.State.STOP -> cancelRecord()
+        AudioStateButton.State.PLAYING -> part3ViewModel.playRecord()
+        AudioStateButton.State.PAUSE -> part3ViewModel.pausePlayRecord()
+    }
+
+    private fun getProblemNumber(): Int =
+        intent.getIntExtra(ProblemGuideActivity.KEY_PROBLEM_NUMBER, 1)
+
+    private fun skipPreparation() {
+        part3ViewModel.skipProgress()
+        prepareNoticePlayer?.pause()
+        if (beepPlayer?.isPlaying == true) {
+            beepPlayer?.pause()
+            beepPlayer?.seekTo(0)
+        }
+    }
+
+    private fun startProblem() {
+        problemPlayer = MediaPlayer().apply {
+            setDataSource(part3ViewModel.getProblemSoundUrl())
+            prepare()
+            setOnCompletionListener { startPreparation() }
+        }
+        problemPlayer?.start()
+    }
+
+    private fun startPreparation() {
+        binding?.zoneTimer?.visibility = View.VISIBLE
+        rewindProgressBar(PREPARATION_TIME)
+        playSound(prepareNoticePlayer) {
+            playSound(beepPlayer) { part3ViewModel.startCountDown(PREPARATION_TIME) }
+        }
+        part3ViewModel.setOnProgressFinishListener { startSpeakingTime() }
+    }
+
+    private fun startSpeakingTime() {
+        part3ViewModel.changeState(ProblemState.RESPONSE)
+        rewindProgressBar(RESPONSE_TIME_15)
+        playSound(speakingNoticePlayer) {
+            playSound(beepPlayer) { startRecord(RESPONSE_TIME_15) }
+        }
+    }
+
+    private fun rewindProgressBar(maxProgress: Int) {
+        requireBinding().progressBar.maxProgress = maxProgress
+        part3ViewModel.resetProgress()
+    }
+
+    private fun startRecord(duration: Int) {
+        part3ViewModel.prepareRecorder(getExternalDirectoryPath())
+        part3ViewModel.startRecord(duration)
+        part3ViewModel.setOnProgressFinishListener {
+            part3ViewModel.finishRecord()
+            playSound(beepPlayer)
+            deployStopTalkingDialog()
+        }
+    }
+
+    private fun deployStopTalkingDialog() {
+        StopTalkingButtonsDialog(this).apply {
+            setOnCheckProblemClickListener { listenMyRecord() }
+            setOnNextClickListener { startNextProblem() }
+        }.show()
+//        part6ViewModel.saveSolvedProblem()
+        //FIXME 나중에 푼 문제 저장되는것 주석 해제 할 것.
+    }
+
+    private fun listenMyRecord() {
+        part3ViewModel.changeState(ProblemState.MY_RECORD)
+        requireBinding().progressBar.initToProgressBar(part3ViewModel.getRecordDuration())
+        requireBinding().progressBar.setOnProgressChangeListener { part3ViewModel.playRecord(it) }
+        part3ViewModel.playRecord(0)
+    }
+
+    private fun cancelRecord() {
+        part3ViewModel.cancelRecord()
+        speakingNoticePlayer?.pause()
+        if (beepPlayer?.isPlaying == true) {
+            beepPlayer?.pause()
+            beepPlayer?.seekTo(0)
+        }
+    }
+
+    private fun startNextProblem() {
+        val intent = Intent(this, Part6Activity::class.java)
+        intent.putExtra(ProblemGuideActivity.KEY_PROBLEM_NUMBER, getProblemNumber() + 1)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun requireBinding() = binding ?: error("root view must be inflated")
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+        problemPlayer?.release()
+        problemPlayer = null
+        speakingNoticePlayer?.release()
+    }
+
+    companion object {
+        private val PREPARATION_TIME = TimeUnit.SECONDS.toMillis(3).toInt()
+        private val RESPONSE_TIME_15 = TimeUnit.SECONDS.toMillis(4).toInt()
+        private val RESPONSE_TIME_30 = TimeUnit.SECONDS.toMillis(5).toInt()
+    }
+}
---
+++ app/src/main/java/com/tost/presentation/problem/part3/Part3ViewModel.kt
@@ -0,0 +1,63 @@
+package com.tost.presentation.problem.part3
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.tost.data.entity.Part
+import com.tost.data.entity.Problem
+import com.tost.data.entity.ProblemState
+import com.tost.data.repository.ProblemsRepository
+import com.tost.data.repository.RecordsRepository
+import com.tost.data.repository.UserRepository
+import com.tost.presentation.problem.base.AudioViewModel
+import com.tost.presentation.utils.printLog
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+/**
+ * Created By Malibin
+ * on 1월 08, 2021
+ */
+
+class Part3ViewModel @ViewModelInject constructor(
+    recordsRepository: RecordsRepository,
+    private val userRepository: UserRepository,
+    private val problemsRepository: ProblemsRepository,
+) : AudioViewModel(recordsRepository) {
+
+    override val part: Part = Part.THREE
+
+    private val _problem = MutableLiveData<Problem>()
+    val problem: LiveData<Problem> = _problem
+
+    private val _problemState = MutableLiveData(ProblemState.PREPARE)
+    val problemState: LiveData<ProblemState> = _problemState
+
+    fun loadProblem(problemNumber: Int) = viewModelScope.launch {
+        changeLoadingTo(true)
+        val tostToken = userRepository.getTostToken()
+        _problem.value = if (tostToken == null) {
+            problemsRepository.getTrialProblemInfo(part)
+        } else {
+            problemsRepository.getProblemInfo(tostToken, part, problemNumber)
+        }
+        printLog(_problem.value.toString())
+        changeLoadingTo(false)
+    }
+
+    fun getProblemSoundUrl(): String {
+        return _problem.value?.audioUrl ?: error("audioUrl Not Exist")
+    }
+
+    fun changeState(state: ProblemState) {
+        _problemState.value = state
+    }
+
+    fun saveSolvedProblem() = CoroutineScope(Dispatchers.IO).launch {
+        val tostToken = userRepository.getTostToken() ?: return@launch
+        val currentProblemNumber = _problem.value?.questionNumber ?: error("problem is null")
+        problemsRepository.saveSolvedProblem(tostToken, part, currentProblemNumber)
+    }
+}
