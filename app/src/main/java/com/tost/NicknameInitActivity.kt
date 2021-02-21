d5ec2a214cd1276c3c178527e989912452b923a0
Malibin <nightmare73@naver.com>
2021-02-21 23:59:03 +0900

    [Feat] 회원가입 처음 Nickname 받는 Layout 작성

---
+++ app/src/main/java/com/tost/NicknameInitActivity.kt
@@ -0,0 +1,11 @@
+package com.tost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NicknameInitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nickname_init)
+    }
+}
